3b42e6f2cf791cb3163ee3b591e97e9f49762995
medice <toni.nisumaa@gmail.com>
2017-02-10 23:44:34 +0200

    more concise

--- src/router.js
+++ src/router.js
@@ -3,23 +3,36 @@ import VueRouter from 'vue-router/dist/vue-router.js'
 
 Vue.use(VueRouter)
 
-// These components don't work. 
-import Home from './App.vue'
-import Hello from './components/Hello.vue'
-
-const home = Home;
-const hello = Hello;
-
-// The below components work.
-import test from './test.js'
-const Foo = { template: '<div>foo</div>' }
+// import hello from './components/Hello.vue'
+const Bar = {
+    template: '<input v-model="foo"></input>',
+    data() {
+        return {
+            foo: "bar"
+        }
+    }
+}
+const Foo = { template: '<input v-model="foo"></input>',
+    data() {
+        return {
+            foo: "foo"
+        }
+    },
+    components: {
+        Bar
+    }
+}
+const Dummy = {
+    template: '<div></div>'
+}
 
 const router = new VueRouter({
     mode: 'history',
     base: __dirname,
     routes: [
-        { path: '/', name: 'home', component: Home},     
-        {path: '/hello', name: 'hello', component: Hello}  
+        {path: '/', name: 'foo', component: Bar},     
+        {path: '/Foo', name: 'bar', component: Foo},
+        {path: '/bah', component: Dummy}
     ]
 })
 
